6c31cbc69a7af5a30e97bd340a204043bbe1b037
vkuzel <vkuzel@gmail.com>
2020-10-25 17:42:39 +0100

    * Added API renderToByteArray methods.

--- src/main/java/com/github/vkuzel/simplepdflayout/Document.java
+++ src/main/java/com/github/vkuzel/simplepdflayout/Document.java
@@ -4,9 +4,34 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public final class Document {
+
+    public static byte[] renderPagesToByteArray(Pages pages) {
+        try (PDDocument pdDocument = new PDDocument()) {
+            for (Page page : pages.getPages()) {
+                renderPage(pdDocument, page);
+            }
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            pdDocument.save(outputStream);
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static byte[] renderPageToByteArray(Page page) {
+        try (PDDocument pdDocument = new PDDocument()) {
+            renderPage(pdDocument, page);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            pdDocument.save(outputStream);
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     public static void renderPagesToFile(Pages pages, String fileName) {
         try (PDDocument pdDocument = new PDDocument()) {
@@ -22,6 +47,7 @@ public final class Document {
     public static void renderPageToFile(Page page, String fileName) {
         try (PDDocument pdDocument = new PDDocument()) {
             renderPage(pdDocument, page);
+            pdDocument.save(fileName);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
